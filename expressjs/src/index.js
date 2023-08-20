9ea8d9658feef4470cc6c037d1578d3b49f8b9b6
MANISHA NAYAK <nayakmanisha515@gmail.com>
2023-08-20 22:08:19 +0530

    serve html css and js file in express js

--- expressjs/src/index.js
+++ expressjs/src/index.js
@@ -1,6 +1,15 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 const port = 3000
+
+// console.log(__dirname);
+
+// console.log(path.join(__dirname, '../public'))
+const staticPath = path.join(__dirname, '../public')
+
+// built in middleware
+app.use(express.static(staticPath));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
