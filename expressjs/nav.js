73e040ce3bf79466e1780e695d7e52c660125471
MANISHA NAYAK <nayakmanisha515@gmail.com>
2023-08-20 20:45:00 +0530

    send html and json object as a response using express

--- expressjs/nav.js
+++ expressjs/nav.js
@@ -1,19 +1,35 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('welcom to my home page')
-})
+app.get("/", (req, res) => {
+  res.write("<h1>welcom to my home page</h1>");
+  res.write("<h1>welcom to my home page again</h1>");
+  res.send();
+});
 
-app.get('/about', (req, res) => {
-  res.send('welcom to my about page')
-})
+app.get("/about", (req, res) => {
+  res.send("welcom to my about page");
+});
 
-app.get('/contact', (req, res) => {
-    res.send('welcom to my contact page')
-  })
+app.get("/contact", (req, res) => {
+  // res.send('welcom to my contact page')
+  res.send([
+    {
+      id: 1,
+      name: "manisha",
+    },
+    {
+      id: 2,
+      name: "aditya",
+    },
+    {
+      id: 3,
+      name: "vidhi",
+    },
+  ]);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
